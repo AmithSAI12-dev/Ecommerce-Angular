76ffe8086765cada0e45df50b0d2c7629634766f
Amith <amithsai78@gmail.com>
2021-06-15 19:31:21 +0530

    Added mobile-header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './pages/home/home.component';
+import { MobileHeaderComponent } from './components/header/mobile-header/mobile-header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    MobileHeaderComponent
   ],
   imports: [
     BrowserModule,
